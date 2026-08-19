a166f5b88a4ba956613d72550243f07e71cd4fee
Pasang Tamang <pasanghilp@gmail.com>
2026-08-19 18:33:20 +0545

    Refactor BottomRow component to manage task state and display remaining tasks

--- src/Components/BottomRow.jsx
+++ src/Components/BottomRow.jsx
@@ -1,11 +1,23 @@
 import "./BottomRow.css";
 
-export function BottomRow() {
+export function BottomRow({ task, setTask }) {
+    const updated = task.filter((taskInput) => !taskInput.done);
+    const deleteCompleted = () => {
+        setTask(updated);
+    };
     return (
         <>
             <div className="bottom-row">
-                <span className="task-count" id="taskCount"></span>
-                <button className="clear-btn" id="clearBtn">
+                <span className="task-count" id="taskCount">
+                    {updated.length === 0
+                        ? "All Done!"
+                        : `${updated.length} task remaining`}
+                </span>
+                <button
+                    className="clear-btn"
+                    id="clearBtn"
+                    onClick={deleteCompleted}
+                >
                     🗑 Clear completed
                 </button>
             </div>
