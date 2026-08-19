a148d584a6c64ac492b4ac34ae42fbc3ed22e067
Pasang Tamang <pasanghilp@gmail.com>
2026-08-19 18:33:31 +0545

    Update Stats component to display task statistics based on priority and completion status

--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -1,30 +1,47 @@
 import "./Stats.css";
 
-export function Stats() {
+export function Stats({ task }) {
+    const taskLength = task.length;
+    const taskHigh = task.filter(
+        (taskItem) => taskItem.priority === "high",
+    ).length;
+    const taskMed = task.filter(
+        (taskItem) => taskItem.priority === "med",
+    ).length;
+    const tasklow = task.filter(
+        (taskItem) => taskItem.priority === "low",
+    ).length;
+    const taskDone = task.filter((taskItem) => taskItem.done).length;
     return (
         <>
             <div className="stats">
                 <div className="stat">
                     <span className="stat-num" id="stat-total">
-                        0
+                        {taskLength}
                     </span>
                     <span className="stat-label">Total</span>
                 </div>
                 <div className="stat high">
                     <span className="stat-num" id="stat-high">
-                        0
+                        {taskHigh}
                     </span>
                     <span className="stat-label">High</span>
                 </div>
                 <div className="stat med">
                     <span className="stat-num" id="stat-med">
-                        0
+                        {taskMed}
                     </span>
                     <span className="stat-label">Medium</span>
                 </div>
+                <div className="stat low">
+                    <span className="stat-num" id="stat-low">
+                        {tasklow}
+                    </span>
+                    <span className="stat-label">low</span>
+                </div>
                 <div className="stat done">
                     <span className="stat-num" id="stat-done">
-                        0
+                        {taskDone}
                     </span>
                     <span className="stat-label">Done</span>
                 </div>
